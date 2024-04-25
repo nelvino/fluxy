305c63681a792b5e8e5b0d1c1d3d3e42c8ea9649
Nico Elvino <nico.elvino@triiyo.com>
2024-04-25 20:39:03 -0300

    whatsapp icon position

--- src/components/WhatsAppIcon.jsx
+++ src/components/WhatsAppIcon.jsx
@@ -7,7 +7,7 @@ const WHATSAPP_URL = "https://wa.me/+5491136949578"; // Our mobile number
 
 const WhatsAppIcon = () => {
   return (
-    <Link to={WHATSAPP_URL} target="_blank" className="fixed bottom-10 right-10 z-50 rounded-full bg-green-500 hover:bg-green-700 shadow-md p-2 flex justify-center items-center">
+    <Link to={WHATSAPP_URL} target="_blank" className="fixed bottom-5 sm:bottom-10 right-3 sm:right-10 z-50 rounded-full bg-green-500 hover:bg-green-700 shadow-md p-2 flex justify-center items-center">
       {/* <i className="fab fa-whatsapp text-white text-xl"></i> */}
       <img width={32} height={32} src={WhatsAppIcon96} />
     </Link>
